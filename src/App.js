986be05b8521d8318f95cc862809dcc466f1e8a0
Kirill Ginko <kirillginko@gmail.com>
2022-03-16 14:27:44 -0400

    added cursor effects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
+import CursorEffect from "./components/CursorEffect";
 
 const FPS = 60;
 const speed = 2;
@@ -72,18 +73,24 @@ export default function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1
-        className="logo"
-        style={{
-          top: dvdPos.top + "px",
-          left: dvdPos.left + "px",
-          color: dvdColor.current,
-        }}
-      >
-        🖼️🖼️🖼️
-      </h1>
-      {/* <svg
+    <>
+      <CursorEffect
+        element={document.querySelector("App")}
+        type="ghost"
+      ></CursorEffect>
+      <div className="App">
+        <div className="container"></div>
+        <h1
+          className="logo"
+          style={{
+            top: dvdPos.top + "px",
+            left: dvdPos.left + "px",
+            color: dvdColor.current,
+          }}
+        >
+          🖼️🖼️🖼️
+        </h1>
+        {/* <svg
         className="logo"
         viewBox="0 0 16 8"
         fill="none"
@@ -96,7 +103,7 @@ export default function App() {
       >
         <path d="M7.4 4.959C3.268 4.959 0 5.509 0 6.186C0 6.864 3.268 7.413 7.4 7.413C11.532 7.413 14.943 6.864 14.943 6.186C14.944 5.508 11.533 4.959 7.4 4.959ZM7.263 6.51C6.306 6.51 5.53 6.273 5.53 5.98C5.53 5.687 6.306 5.45 7.263 5.45C8.22 5.45 8.995 5.687 8.995 5.98C8.995 6.273 8.219 6.51 7.263 6.51ZM13.319 0.052002H9.701L7.769 2.291L6.849 0.0830021H1.145L0.933 1.045H3.202C3.202 1.045 4.239 0.909002 4.273 1.739C4.273 3.177 1.897 3.055 1.897 3.055L2.341 1.555H0.869L0.194 3.988H2.862C2.862 3.988 5.683 3.738 5.683 1.77C5.683 1.77 5.797 1.196 5.749 0.943002L7.124 4.62L10.559 1.055H13.165C13.165 1.055 13.963 1.123 13.963 1.74C13.963 3.178 11.604 3.028 11.604 3.028L11.969 1.556H10.682L9.946 3.989H12.399C12.399 3.989 15.465 3.799 15.465 1.71C15.465 1.709 15.404 0.052002 13.319 0.052002Z" />
       </svg> */}
-      {/* <svg
+        {/* <svg
         style={{
           top: dvdPos.top + "px",
           left: dvdPos.left + "px",
@@ -149,6 +156,7 @@ export default function App() {
           </linearGradient>
         </defs>
       </svg> */}
-    </div>
+      </div>
+    </>
   );
 }
